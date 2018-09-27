6bf309d76b026a2fa2964848c53824e66e4805eb
Alexander Viklund <viklu@kth.se>
2018-09-27 03:35:18 +0200

    Implement Lexer Iterator

--- src/main/scala/punkt0/lexer/Lexer.scala
+++ src/main/scala/punkt0/lexer/Lexer.scala
@@ -2,24 +2,30 @@ package punkt0
 package lexer
 
 import java.io.File
+import scala.collection.mutable.ListBuffer
 
 
 object Lexer extends Phase[File, Iterator[Token]] {
   import Reporter._
 
+
   def run(f: File)(ctx: Context): Iterator[Token] = {
     val source = scala.io.Source.fromFile(f)
+    val tokens = new ListBuffer[Token]
 
     // TODO: implement this method
 
+    var tokenList = tokens.toList
     new Iterator[Token] {
 
-      def hasNext = {
-        ???
+      def hasNext: Boolean = {
+        tokenList.nonEmpty
       }
 
-      def next = {
-        ???
+      def next: Token = {
+        val token = tokenList.head
+        tokenList = tokenList.tail
+        token
       }
     }
   }
