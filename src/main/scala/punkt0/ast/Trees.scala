c44f582ada6a0179ee66634941bfbddded60a1c6
Michel Tabari <tabari@kth.se>
2018-10-29 11:51:22 +0100

    Fix formating

--- src/main/scala/punkt0/ast/Trees.scala
+++ src/main/scala/punkt0/ast/Trees.scala
@@ -5,24 +5,27 @@ import analyzer.Symbols._
 
 object Trees {
   val indent_length = 2
+
   sealed trait Tree extends Positioned {
     def prettyPrint(sb: StringBuilder, indent: Int): Unit
   }
 
   case class Program(main: MainDecl, classes: List[ClassDecl]) extends Tree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
-      classes.foreach(c => {c.prettyPrint(sb, indent); sb.append("\n\n")})
+      classes.foreach(c => {
+        c.prettyPrint(sb, indent); sb.append("\n\n")
+      })
       main.prettyPrint(sb, indent)
       sb.append("\n")
     }
   }
 
-  case class MainDecl(obj: Identifier, parent: Identifier, vars: List[VarDecl], exprs: List[ExprTree]) extends Tree  with Symbolic[ClassSymbol] {
-    override def prettyPrint(sb: StringBuilder, indent: Int): Unit= {
-      sb.append(" "*indent).append("object ").append(obj.value).append(" extends ").append(parent.value).append(" {\n")
+  case class MainDecl(obj: Identifier, parent: Identifier, vars: List[VarDecl], exprs: List[ExprTree]) extends Tree with Symbolic[ClassSymbol] {
+    override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
+      sb.append(" " * indent).append("object ").append(obj.value).append(" extends ").append(parent.value).append(" {\n")
       val indent2 = indent + 2
       for (var_ <- vars) {
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         var_.prettyPrint(sb, indent2)
         sb.append("\n")
       }
@@ -36,13 +39,13 @@ object Trees {
         }
       }
       sb.append("\n")
-      sb.append(" "*indent).append("}")
+      sb.append(" " * indent).append("}")
     }
   }
 
   case class ClassDecl(id: Identifier, parent: Option[Identifier], vars: List[VarDecl], methods: List[MethodDecl]) extends Tree with Symbolic[ClassSymbol] {
-    override def prettyPrint(sb: StringBuilder, indent: Int): Unit= {
-      sb.append(" "*indent).append("class ").append(id.value)
+    override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
+      sb.append(" " * indent).append("class ").append(id.value)
       parent match {
         case Some(p) => sb.append(" extends ").append(p.value)
         case None => Unit
@@ -50,7 +53,7 @@ object Trees {
       sb.append(" {\n")
       val indent2 = indent + indent_length
       for (var_ <- vars) {
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         var_.prettyPrint(sb, indent2)
         sb.append("\n")
       }
@@ -59,12 +62,12 @@ object Trees {
         meth.prettyPrint(sb, indent2)
         sb.append("\n\n")
       }
-      sb.append(" "*indent).append("}")
+      sb.append(" " * indent).append("}")
     }
   }
 
-  case class VarDecl(tpe: TypeTree, id: Identifier, expr: ExprTree) extends Tree  with Symbolic[VariableSymbol]{
-    override def prettyPrint(sb: StringBuilder, indent: Int): Unit= {
+  case class VarDecl(tpe: TypeTree, id: Identifier, expr: ExprTree) extends Tree with Symbolic[VariableSymbol] {
+    override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("var ").append(id.value).append(": ")
       tpe.prettyPrint(sb, indent)
       sb.append(" = ")
@@ -73,8 +76,8 @@ object Trees {
     }
   }
 
-  case class MethodDecl(overrides: Boolean, retType: TypeTree, id: Identifier, args: List[Formal], vars: List[VarDecl], exprs: List[ExprTree], retExpr: ExprTree) extends Tree  with Symbolic[MethodSymbol]{
-    override def prettyPrint(sb: StringBuilder, indent: Int): Unit= {
+  case class MethodDecl(overrides: Boolean, retType: TypeTree, id: Identifier, args: List[Formal], vars: List[VarDecl], exprs: List[ExprTree], retExpr: ExprTree) extends Tree with Symbolic[MethodSymbol] {
+    override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       if (overrides)
         sb.append("override ")
       sb.append("def ").append(id.value).append("(")
@@ -90,20 +93,20 @@ object Trees {
       sb.append(" = {\n")
       val indent2 = indent + indent_length
       for (var_ <- vars) {
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         var_.prettyPrint(sb, indent2)
         sb.append("\n")
       }
       for (expression <- exprs) {
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         expression.prettyPrint(sb, indent2)
         sb.append(";\n")
       }
       sb.dropRight(2).append("\n")
-      sb.append(" "* indent2)
+      sb.append(" " * indent2)
       retExpr.prettyPrint(sb, indent2)
       sb.append("\n")
-      sb.append(" "*indent).append("}")
+      sb.append(" " * indent).append("}")
     }
   }
 
@@ -115,6 +118,7 @@ object Trees {
   }
 
   sealed trait TypeTree extends Tree
+
   case class BooleanType() extends TypeTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("Boolean")
@@ -140,7 +144,7 @@ object Trees {
 
 
   sealed trait ExprTree extends Tree
-  
+
   case class And(lhs: ExprTree, rhs: ExprTree) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       lhs.prettyPrint(sb, indent)
@@ -224,6 +228,7 @@ object Trees {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append(value)
   }
+
   case class StringLit(value: String) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("\"").append(value).append("\"")
@@ -233,26 +238,32 @@ object Trees {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("true")
   }
+
   case class False() extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("false")
   }
+
   case class Identifier(value: String) extends TypeTree with ExprTree with Symbolic[Symbol] {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append(value)
   }
+
   case class This() extends ExprTree with Symbolic[ClassSymbol] {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("this")
   }
+
   case class Null() extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("null")
   }
+
   case class New(tpe: Identifier) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit =
       sb.append("new ").append(tpe.value).append("()")
   }
+
   case class Not(expr: ExprTree) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("!")
@@ -279,6 +290,7 @@ object Trees {
       }
     }
   }
+
   case class If(expr: ExprTree, thn: ExprTree, els: Option[ExprTree]) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("if (")
@@ -289,7 +301,7 @@ object Trees {
         thn.prettyPrint(sb, indent2)
       } else {
         sb.append("\n")
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         thn.prettyPrint(sb, indent2)
       }
       els match {
@@ -299,13 +311,14 @@ object Trees {
             e.prettyPrint(sb, indent2)
           } else {
             sb.append("\n")
-            sb.append(" "*indent2)
+            sb.append(" " * indent2)
             e.prettyPrint(sb, indent2)
           }
         case None => Unit
       }
     }
   }
+
   case class While(cond: ExprTree, body: ExprTree) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("while (")
@@ -316,11 +329,12 @@ object Trees {
         body.prettyPrint(sb, indent)
       } else {
         sb.append("\n")
-        sb.append(" "*indent2)
+        sb.append(" " * indent2)
         body.prettyPrint(sb, indent2)
       }
     }
   }
+
   case class Println(expr: ExprTree) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append("println(")
@@ -328,6 +342,7 @@ object Trees {
       sb.append(")")
     }
   }
+
   case class Assign(id: Identifier, expr: ExprTree) extends ExprTree {
     override def prettyPrint(sb: StringBuilder, indent: Int): Unit = {
       sb.append(id.value)
@@ -335,4 +350,5 @@ object Trees {
       expr.prettyPrint(sb, indent)
     }
   }
+
 }
