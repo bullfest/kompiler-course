bf6189d23401ee2c36ff4d1813dce76fb263ca32
Alexander Viklund <viklu@kth.se>
2018-10-29 12:48:15 +0100

    Add types for member declarations

--- src/main/scala/punkt0/analyzer/Symbols.scala
+++ src/main/scala/punkt0/analyzer/Symbols.scala
@@ -34,15 +34,15 @@ object Symbols {
 
   class GlobalScope {
     var mainClass: ClassSymbol = _
-    var classes = Map[String, ClassSymbol]()
+    var classes: Map[String, ClassSymbol] = Map[String, ClassSymbol]()
 
     def lookupClass(n: String): Option[ClassSymbol] = classes.get(n)
   }
 
   class ClassSymbol(val name: String) extends Symbol {
     var parent: Option[ClassSymbol] = None
-    var methods = Map[String, MethodSymbol]()
-    var members = Map[String, VariableSymbol]()
+    var methods: Map[String, MethodSymbol] = Map[String, MethodSymbol]()
+    var members: Map[String, VariableSymbol] = Map[String, VariableSymbol]()
 
     def lookupMethod(n: String): Option[MethodSymbol] = methods.get(n) match {
       case m @ Some(_) => m
@@ -62,19 +62,18 @@ object Symbols {
   }
 
   class MethodSymbol(val name: String, val classSymbol: ClassSymbol) extends Symbol {
-    var params = Map[String, VariableSymbol]()
-    var members = Map[String, VariableSymbol]()
+    var params: Map[String, VariableSymbol] = Map[String, VariableSymbol]()
+    var members: Map[String, VariableSymbol] = Map[String, VariableSymbol]()
     var argList: List[VariableSymbol] = Nil
     var overridden: Option[MethodSymbol] = None
 
     def lookupVar(n: String): Option[VariableSymbol] = members.get(n) match {
       case v @ Some(_) => v
-      case None => {
+      case None =>
         params.get(n) match {
           case v @ Some(_) => v
           case None => classSymbol.lookupVar(n)
         }
-      }
     }
   }
 
