c9be0410f697e15dadc99e39086c95b12e0c884a
Alexander Viklund <viklu@kth.se>
2018-10-30 01:29:54 +0100

    Collect all symbols in name analysis

--- src/main/scala/punkt0/analyzer/NameAnalysis.scala
+++ src/main/scala/punkt0/analyzer/NameAnalysis.scala
@@ -2,7 +2,6 @@ package punkt0
 package analyzer
 
 import ast.Trees._
-import Symbols._
 
 object NameAnalysis extends Phase[Program, Program] {
 
@@ -13,12 +12,13 @@ object NameAnalysis extends Phase[Program, Program] {
 
     globalScope.mainClass = prog.main.collectSymbol
 
+    prog.classes.foreach(cls => globalScope.classes += (cls.id.value -> cls.collectSymbol))
+
     // Step 1: Collect symbols in declarations
     // Step 2: Attach symbols to identifiers (except method calls) in method bodies
     // (Step 3:) Print tree with symbol ids for debugging
 
     // Make sure you check all constraints
-
     prog
   }
 
