7b29748ea1282c920b4a360db0f4b6d8915c247f
Michel Tabari <tabari@kth.se>
2018-11-09 23:00:04 +0100

    Null is not subtype of string

--- src/main/scala/punkt0/analyzer/Types.scala
+++ src/main/scala/punkt0/analyzer/Types.scala
@@ -99,7 +99,6 @@ object Types {
   case object TNull extends Type {
     override def isSubTypeOf(tpe: Type): Boolean = tpe match {
       case TNull => true
-      case TString => true
       case TClass(_) => true
       case _ => false
     }
